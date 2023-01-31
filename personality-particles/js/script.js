471430b6a0d4a37b7cc077384b66666eb62e3e9d
ChthonicCandelabra <122397646+ChthonicCandelabra@users.noreply.github.com>
2023-01-31 13:01:33 -0600

    Added new code
    Updated with in-progress code

--- personality-particles/js/script.js
+++ personality-particles/js/script.js
@@ -1,17 +1,37 @@
 /**
-Tic Tac Toe 
-Leah
+Personality Particle: Breaking Up With a Lover </3
+Leah Fawcett
 
-Coding challenge for class.
 */
 
 "use strict";
 
+let particle = []; // set up the petal object to make flower shapes
+
 function setup() {
-  createCanvas(500, 600);
-  background(0, 0 , 0);
+  createCanvas(700, 700); // makes the canvas
+  background(255, 121, 184);
+
+  for (let i = 0; i < 50; i++) { // sets the max amount of petals to generate
+    particle[i] = new Petal(100, 10, 10); // lets the computer know we will be making a new object
+  }
 }
 
 function draw() {
+  for (let i = 0; i < 50; i++) { // sets the max amount of petals to generate
+    particle[i].display(); // 
+  }
+}
 
+class Petal { // creates an object class for petals
+  constructor(x, y, r) {
+    this.x = random(0, 700); // places the leaves along the x value range
+    this.y = random(0, 700); // places the leaves along the y value range
+    this.r = random(20, 200); // decides radius value
+  }
+
+   display() {
+    fill(0, 0, 0);
+    ellipse(this.x, this.y);
+  }
 }
