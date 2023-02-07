86144d06bf43a2ee3e06b88b833d4f1ac8290de1
ChthonicCandelabra <122397646+ChthonicCandelabra@users.noreply.github.com>
2023-02-07 13:09:01 -0600

    Particle code
    Added particle project code, needs polish

--- personality-particles/js/script.js
+++ personality-particles/js/script.js
@@ -2,41 +2,113 @@
 Personality Particle: Breaking Up With a Lover </3
 Leah F
 
+//https://www.youtube.com/watch?v=TaN5At5RWH8
+
 */
 
 "use strict";
 
+let turn = 0; 
+
 let particle = []; // set up the petal object to make flower shapes
 
 function setup() {
-  createCanvas(700, 700); // makes the canvas
+  createCanvas(windowWidth, windowHeight); // size canvas depending on window dimensions
 
-  for (let i = 0; i < 1; i++) { // sets the max amount of petals to generate
-    particle[i] = new Petal(); // lets the computer know we will be making a new object
+  // for (let i = 0; i < 10; i++) { // sets the max amount of petals to generate
+  //   particle[i] = new Petal(200, 200, 40); // lets the computer know we will be making a new object
+  // }
+}
 
+function mousePressed() {
+  // let place = new Petal(mouseX, mouseY, 100)
+  // particle.push(place);
+
+  turn++; // Increases the turn counter by 1 per click.
+  print(turn);
+
+  for (let i = 0; i < particle.length; i++) {
+    particle[i].clicked(mouseX, mouseY);
   }
 }
 
 function draw() {
-  background(66, 47, 53);
-  for (let i = 0; i < 1; i++) { // sets the max amount of petals to generate
-    particle[i].display(); // 
 
+  if (turn%2) { // Changes the background if the turn # is even or odd
+    background(220, 79, 114);
+  } else {
+    background(82, 72, 76);
+  }
+
+  for (let i = 0; i < 1; i++) { // sets the max amount of petals to generate
+    particle.push(new Petal(mouseX, mouseY, 100));
+   
+    particle[i].display(); // calls the particle speficic functions
+    particle[i].move(); //^
+    particle[i].tooFastTooSoon();
+    particle[i].decay(); // 
+    
+  }
+
+  for (let i = particle.length - 1; i >= 0; i--) {
+    if (particle[i].finished()) {
+      particle.splice(i, 1);
+    }
   }
 }
 
 class Petal { // creates an object class for petals
-  constructor(x, y) {
-    this.x = random(50, 650); // places the leaves along the x value range
-    this.y = random(50, 650); // places the leaves along the y value range
-  }
+  constructor(x, y, r, lifetime) {
+    // this.x = mouseX; // places the petals on the mouse's x position
+    // this.y = mouseY; // places the petals on the mouse's y position
+    this.x = random(0, windowWidth); // places the petals along the x value range
+    this.y = 0; // places the petals along the y value range
+    this.r = 100; // decides radius value
+    this.lifetime = 50;
+    }
 
-   display() {
-    noStroke();
-    fill(255, 0, 0);
-   beginShape();
-    vertex(this.x, this.y);
-    bezierVertex(80, 0, 80, 75, 30, 75);
-    endShape();
-}
-  }
+    display() {
+      //colorMode(HSB, 0, 100, 100) // changes the colour mode of particles to HSB
+      noStroke();
+      let firstColour = color(250, 131, 173); // pink
+      let secondColour = color(255, 0, 0); // red
+      let thirdColour = color(17, 2, 33); // violet
+      let fourthColour = color(0, 0, 0); // black
+      let percent = map(this.lifetime, 50, 0, 0, 1); // maps the lifetime to the percentage value for lerp
+      let colourEffect = lerpColor(firstColour, secondColour, percent); // puts it all together into a usuable variable
+
+      fill(colourEffect);
+      ellipse(this.x, this.y, this.lifetime*2, this.lifetime*4); // makes the particles display as an ellispe
+      // vertex(this.x, this.y);
+      //  bezierVertex(this.x + 10, this.x + 10, 0, this.y + 3, this.x, this.y);
+      // endShape();
+      }
+
+      clicked(ex, why) {
+        let range = dist(ex, why, this.x, this.y);
+        if (range < this.r) {
+          
+        }
+      }
+
+      move() {
+        this.x = this.x + lerp(random(-50, -45), random(45, 50), 0.5); // makes the particles shake with lerp
+        this.y+=5;
+      }
+
+      finished() {
+        return (this.lifetime < 0);
+      }
+
+      decay() {
+        this.lifetime -= 0.1;
+      }
+
+      tooFastTooSoon() {
+        for (let i = particle.length - 1; i >= 0; i--) {
+          if (this.y > windowHeight + 150 ) {
+            particle.splice(i, 1);
+          }
+        }
+      }
+    }
--- template-p5-project/js/script.js
+++ template-p5-project/js/script.js
@@ -16,23 +16,24 @@ let personalGrief= [17, 2, 33];
 let deadRose = [0, 0, 0];
 
 function setup() {
-  createCanvas(500, 500);
+  createCanvas(1000, 1000);
   
 }
 
 function draw() {
 
   background(0, 0, 0);
-  for (let f = 0; f < 5; f++) {
-    resetMatrix();
-    let angle = (TWO_PI/5)*f;
-    translate(
-      sin(angle)*100,
-      cos(angle)*100
-    )
-    beginShape();
-    vertex(mouseX, mouseY);
-    bezierVertex(300, f, 5, 200, mouseX, mouseY);
+
+  fill(honeyMoon);
+
+  translate(width/2, height/2);
+	for (var i = 0; i < 8; i++) {
+		push();
+		rotate(TWO_PI * i / 8);
+    vertex(0, 0);
+     bezierVertex(20, 20, 0, 30, 0, 0);
     endShape();
+
+    pop();
   }
 }
